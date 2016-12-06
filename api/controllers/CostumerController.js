d2825b66d213bf16b866f5005ed0a962592279c5
Jeferson Guardezi <jeferson@guardezi.com>
2016-12-05 22:51:48 -0200

    ajuste no update do telefone

--- api/controllers/CostumerController.js
+++ api/controllers/CostumerController.js
@@ -11,28 +11,35 @@
       return res.badRequest( 'you must pass all parameters: email name ');
     } else {
       var data = req.body;
-      Device.findOne({id:data.device})
-      .then(function (device) {
-        if(device){
-          data.device = device;
-          data = Costumer.prepareData(data);
-          Costumer.create(data)
-          .then(function (costumer) {
-            return res.json(costumer);
-          })
-          .catch(function(error) {
+      if(data.id){
+        Costumer.update({id:data.id},{phone:data.phone})
+        .then(function (result) {
+          return res.json(result[0]);
+        })
+      }else{
+        Device.findOne({id:data.device})
+        .then(function (device) {
+          if(device){
+            data.device = device;
+            data = Costumer.prepareData(data);
+            Costumer.create(data)
+            .then(function (costumer) {
+              return res.json(costumer);
+            })
+            .catch(function(error) {
+              res.status(500);
+              return res.json(error);
+            });
+          }else{
             res.status(500);
-            return res.json(error);
-          });
-        }else{
+            return res.json('device error');
+          }
+        })
+        .catch(function(error) {
           res.status(500);
-          return res.json('device error');
-        }
-      })
-      .catch(function(error) {
-        res.status(500);
-        return res.json(error);
-      });
+          return res.json(error);
+        });
+      }
     }
   },
 }
