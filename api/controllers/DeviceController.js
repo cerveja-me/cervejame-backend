97bdadeff66a0b8b0600a5c0921819a81269df1a
Jeferson Guardezi <guardezi@ambev.com.br>
2017-01-11 21:51:25 -0200

    ajuste no update do push

--- api/controllers/DeviceController.js
+++ api/controllers/DeviceController.js
@@ -14,7 +14,7 @@
       Device.findOne({push_token:req.body.push_token})
       .then(function (device) {
         if(device){
-          Device.update({id:device.id},{id:device.id})
+          Device.update({id:device.id, type:device.type},{id:device.id})
           .then(function (result) {
             return res.json(result[0]);
           })
